7485878c3e9f317e2afac5e372c1ad3d2af8029e
adikajanai <adikajanai@gmail.com>
2024-06-02 09:30:28 +0000

    made changes to ongoing tasks and categories

--- App.js
+++ App.js
@@ -5,55 +5,105 @@ export default function App() {
   return (
     
     <SafeAreaView style={styles.container}>
-      <View >
-       
-          <Text style={styles.TSize} >
-             Hello, devs {" " + " " + " " + " " + " " + " " + " " + " "} 
-            <Image source={require("./Profile_Image.png")} style={styles.image}/>
-          </Text>
+      <View style={styles.flx}>
+          <View flx2>
+            <Text style={styles.TSize} >
+             Hello, Devs 
+            </Text>  
 
-          <Text style={styles.size} >14 tasks today</Text>
-         
+            <Text style={styles.size}>
+             14 tasks today
+            </Text>
+          </View>  
+            <Image source={require("./Profile_Image.png")}/>
       </View>
       
       
-     <TextInput
+      <TextInput
         style={styles.searchBox}
         placeholder="Search"
-     />
+      />
       <ScrollView>
        <View><Text style={styles.size2}> Categories</Text></View>
 
-      <ScrollView horizontal={true} style={styles.horizontalScroll}>
-        <View style={styles.scrollItem1}><Text style={styles.ts1}>Exercise</Text><Text style={styles.ts2}>12 tasks</Text><Image source={require("./young woman working at desk.png")} style={styles.image}/></View>
-        <View style={styles.scrollItem2}><Text style={styles.ts1}>Item 2</Text><Text style={styles.ts2}>12 tasks</Text><Image source={require("./young woman working online.png")} style={styles.image}/></View>
-        <View style={styles.scrollItem3}><Text style={styles.ts1}>Item 3</Text><Text style={styles.ts2}>12 tasks</Text><Image source={require("./Profile_Image.png")} style={styles.image}/></View>
-        <View style={styles.scrollItem4}><Text style={styles.ts1}>Item 1</Text><Text style={styles.ts2}>12 tasks</Text><Image source={require("./Profile_Image.png")} style={styles.image}/></View>
-        <View style={styles.scrollItem5}><Text style={styles.ts1}>Item 2</Text><Text style={styles.ts2}>12 tasks</Text><Image source={require("./Profile_Image.png")} style={styles.image}/></View>
-        <View style={styles.scrollItem6}><Text style={styles.ts1}>Item 3</Text><Text style={styles.ts2}>12 tasks</Text><Image source={require("./Profile_Image.png")} style={styles.image}/></View>
-        <View style={styles.scrollItem7}><Text style={styles.ts1}>Item 1</Text><Text style={styles.ts2}>12 tasks</Text><Image source={require("./Profile_Image.png")} style={styles.image}/></View>
-        <View style={styles.scrollItem8}><Text style={styles.ts1}>Item 2</Text><Text style={styles.ts2}>12 tasks</Text><Image source={require("./Profile_Image.png")} style={styles.image}/></View>
+      <ScrollView horizontal={true} style={styles.horizontalScroll}  showsHorizontalScrollIndicator={false}>
+        <View style={styles.scrollItem1}>
+          <Text style={styles.ts1}>Exercise</Text>
+          <Text style={styles.ts2}>12 tasks</Text>
+          <View style={styles.image}>
+            <Image source={require("./young woman working online.png")} style={styles.img}/>
+          </View>
+        </View>
+        <View style={styles.scrollItem2}>
+          <Text style={styles.ts1}>Item 2</Text>
+          <Text style={styles.ts2}>12 tasks</Text>
+          <View style={styles.image}>
+             <Image source={require("./young woman working at desk.png")} style={styles.img}/>
+          </View>
+        </View>
+        <View style={styles.scrollItem3}>
+          <Text style={styles.ts1}>Item 3</Text>
+          <Text style={styles.ts2}>12 tasks</Text>
+          <View style={styles.image}>
+            <Image source={require("./Profile_Image.png")} style={styles.img}/>
+          </View>
+        </View>
+        <View style={styles.scrollItem4}>
+          <Text style={styles.ts1}>Item 1</Text>
+          <Text style={styles.ts2}>12 tasks</Text>
+          <View style={styles.image}>
+            <Image source={require("./Profile_Image.png")} style={styles.img}/>
+          </View>
+        </View>
+        <View style={styles.scrollItem5}>
+          <Text style={styles.ts1}>Item 2</Text>
+          <Text style={styles.ts2}>12 tasks</Text>
+          <View style={styles.image}>
+            <Image source={require("./Profile_Image.png")} style={styles.img}/>
+          </View>
+        </View>
+        <View style={styles.scrollItem6}>
+          <Text style={styles.ts1}>Item 3</Text>
+          <Text style={styles.ts2}>12 tasks</Text>
+          <View style={styles.image}>
+            <Image source={require("./Profile_Image.png")} style={styles.img}/>
+          </View>
+        </View>
+        <View style={styles.scrollItem7}>
+          <Text style={styles.ts1}>Item 1</Text>
+          <Text style={styles.ts2}>12 tasks</Text>
+          <View style={styles.image}>
+            <Image source={require("./Profile_Image.png")} style={styles.img}/>
+          </View>
+        </View>
+        <View style={styles.scrollItem8}>
+          <Text style={styles.ts1}>Item 2</Text>
+          <Text style={styles.ts2}>12 tasks</Text>
+          <View style={styles.image}>
+            <Image source={require("./Profile_Image.png")} style={styles.img}/>
+          </View>
+        </View>
       </ScrollView>  
 
       <View ><Text style= {styles.OT}>Ongoing Task</Text></View>
       
         
-      <ScrollView style={styles.verticalScroll}>
-        <View style={styles.scrollItem}><Text style={styles.txt}>Item 1</Text></View>
-        <View style={styles.scrollItem}><Text style={styles.txt}>Item 2</Text></View>
-        <View style={styles.scrollItem}><Text style={styles.txt}>Item 3</Text></View>
-        <View style={styles.scrollItem}><Text style={styles.txt}>Item 3</Text></View>
-        <View style={styles.scrollItem}><Text style={styles.txt}>Item 3</Text></View>
-        <View style={styles.scrollItem}><Text style={styles.txt}>Item 3</Text></View>
-        <View style={styles.scrollItem}><Text style={styles.txt}>Item 3</Text></View>
-        <View style={styles.scrollItem}><Text style={styles.txt}>Item 3</Text></View>
-        <View style={styles.scrollItem}><Text style={styles.txt}>Item 3</Text></View>
-        <View style={styles.scrollItem}><Text style={styles.txt}>Item 3</Text></View>
-        <View style={styles.scrollItem}><Text style={styles.txt}>Item 3</Text></View>
-        <View style={styles.scrollItem}><Text style={styles.txt}>Item 3</Text></View>
-        <View style={styles.scrollItem}><Text style={styles.txt}>Item 3</Text></View>
-        <View style={styles.scrollItem}><Text style={styles.txt}>Item 3</Text></View>
-        <View style={styles.scrollItem}><Text style={styles.txt}>Item 3</Text></View>
+      <ScrollView style={styles.verticalScroll} showsVerticalScrollIndicator={false}>
+        <View style={styles.scrollItem}><Text style={styles.txt}>Mobile App Development</Text></View>
+        <View style={styles.scrollItem}><Text style={styles.txt}>Web Development</Text></View>
+        <View style={styles.scrollItem}><Text style={styles.txt}>Push Ups</Text></View>
+        <View style={styles.scrollItem}><Text style={styles.txt}>Documentation</Text></View>
+        <View style={styles.scrollItem}><Text style={styles.txt}>Maintenance</Text></View>
+        <View style={styles.scrollItem}><Text style={styles.txt}>Research</Text></View>
+        <View style={styles.scrollItem}><Text style={styles.txt}>Deployment</Text></View>
+        <View style={styles.scrollItem}><Text style={styles.txt}>Monitoring</Text></View>
+        <View style={styles.scrollItem}><Text style={styles.txt}>Software Design</Text></View>
+        <View style={styles.scrollItem}><Text style={styles.txt}>Code Review</Text></View>
+        <View style={styles.scrollItem}><Text style={styles.txt}>Testing and Debugging</Text></View>
+        <View style={styles.scrollItem}><Text style={styles.txt}>Optimization</Text></View>
+        <View style={styles.scrollItem}><Text style={styles.txt}>Data Analysis and Reporting</Text></View>
+        <View style={styles.scrollItem}><Text style={styles.txt}>Security Audits  </Text></View>
+        <View style={styles.scrollItem}><Text style={styles.txt}>Mentoring and Training</Text></View>
 
 
       </ScrollView>
@@ -70,8 +120,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E8D1BA',
-    paddingLeft: 20,
+    backgroundColor: 'rgba(232, 209, 186, 0.5)',
+  
     
     
   },
@@ -95,10 +145,6 @@ const styles = StyleSheet.create({
 
   horizontalScroll:{
    marginBottom: 50,
-  },
-  image: {
-   
-    
   },
   size: {
     fontSize: 14,
@@ -180,11 +226,31 @@ const styles = StyleSheet.create({
       width: 'fit-to-screen',
       height: 200,
       marginBottom: 20,
+      borderWidth: 1,
       borderColor: '#E8D1BA',
       borderRadius: 30,
     },
     txt: {
-      textAlign: 'center',
-      paddingTop: 90
+      textAlign: 'left',
+      paddingTop: 75,
+      paddingLeft: 50,
+      fontSize: 30,
     },
+    img :{
+      height: 200,
+      width: 200,
+    },
+    image: {
+      paddingLeft: 70,
+      paddingTop:15,
+     },
+     flx: {
+      flex: 0,
+      flexDirection:'row',
+      alignItems: 'center',
+      marginRight: 100,
+     },
+     flx2: {
+       
+     },
 });
